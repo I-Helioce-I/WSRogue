014fdf4c1428f4d7e80cb96fb435b605d36babf7
Helioce <88891978+I-Helioce-I@users.noreply.github.com>
2024-02-06 11:25:35 +0100

    ADD Player prefabs

--- WSRogue/Assets/Scripts/PlayerController.cs
+++ WSRogue/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class PlayerController : MonoBehaviour
 {
     // Components
     PlayerMovement pM;
 
+    [Header("Stats")]
+    private float currentHealth;
+    private float maxHealth;
+
+
+    private void Awake()
+    {
+        pM = GetComponent<PlayerMovement>();
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damageIn)
+    {
+        currentHealth -= damageIn;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+
+    private void Die()
+    {
+        // ADD stuff to die
+
+    }
 }

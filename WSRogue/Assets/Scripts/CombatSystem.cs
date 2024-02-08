324b9c21a5808d183601fa8c9117a6b9a6d71bd5
Helioce <88891978+I-Helioce-I@users.noreply.github.com>
2024-02-08 15:45:20 +0100

    RETAKE Prefabs / ADD CombatSystem

--- WSRogue/Assets/Scripts/CombatSystem.cs
+++ WSRogue/Assets/Scripts/CombatSystem.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum ComboState
+{
+    First,
+    Second,
+    Third
+}
 public class CombatSystem : MonoBehaviour
 {
-    int actualCombo;
+    PlayerController pC;
+    Animator animator;
 
+    [SerializeField] ComboState currentCombo;
+
+
+    private void Start()
+    {
+        pC = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
+    }
     public void OnAttack(InputAction.CallbackContext context)
     {
-
+        switch (currentCombo)
+        {
+            case ComboState.First:
+                animator.SetTrigger("IsAttacking");
+                break;
+            case ComboState.Second:
+                break;
+            case ComboState.Third:
+                break;
+            default:
+                break;
+        }
 
     }
 }
